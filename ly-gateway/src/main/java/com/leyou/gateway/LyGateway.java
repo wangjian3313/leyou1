fafde46282719860383eba2d75cdf858f4f5e2eb
启 <2364352566@qq.com>
2021-07-12 20:59:53 +0800

    项目搭建

---
+++ ly-gateway/src/main/java/com/leyou/gateway/LyGateway.java
@@ -0,0 +1,11 @@
+package com.leyou.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class LyGateway {
+    public static void main(String[] args) {
+        SpringApplication.run(LyGateway.class,args);
+    }
+}
---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/web/ItemController.java
@@ -0,0 +1,29 @@
+package com.leyou.item.web;
+
+import com.leyou.item.entity.Item;
+import com.leyou.item.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("item")
+public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @PostMapping
+    public ResponseEntity<Item> saveItem(Item item) {
+        //  System.out.println("发送吗"+item);
+
+        Item result = itemService.saveItem(item);
+        // 新增成功, 返回201
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+}
